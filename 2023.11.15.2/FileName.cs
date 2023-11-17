5bee669e2f99ffaac3225460397f2861f1b2bc98
FrostKim0208 <admiral0208@naver.com>
2023-11-17 13:28:32 +0900

    personal

---
+++ 2023.11.15.2/FileName.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023._11._15._2
+{
+    internal class FileName
+    {
+        public enum Month
+        {
+            January = 1,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+
+        static void ProcessMonth(int month)
+        {
+            if (month >= (int)Month.January && month <= (int)Month.December)
+            {
+                Month selectedMonth = (Month)month;
+                Console.WriteLine("선택한 월은 {0}입니다.", selectedMonth);
+            }
+            else
+            {
+                Console.WriteLine("올바른 월을 입력해주세요.");
+            }
+        }
+
+        static void Main()
+        {
+            int userInput = 7; 
+            ProcessMonth(userInput);
+        }
+
+    }
+}
---
+++ 2023.11.15/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _2023._11._15
+{
+    internal class Program
+    {
+        public interface IMovable
+        {
+            void Move(int x, int y); // 이동 메서드 선언
+        }
+        public class Player : IMovable
+        {
+            public void Move(int x, int y)
+            {
+                // 플레이어의 이동 구현
+            }
+        }
+
+        public class Enemy : IMovable
+        {
+            public void Move(int x, int y)
+            {
+                // 적의 이동 구현
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IMovable movableObject1 = new Player();
+            IMovable movableObject2 = new Enemy();
+
+            movableObject1.Move(5, 0); // 플레이어 이동
+            movableObject2.Move(1, 9); // 적 이동
+        }
+    }
+}
